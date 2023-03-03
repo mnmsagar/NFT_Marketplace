059ee84401e96cdd076c48d671ae1ea304002834
mnmsagar <54708901+mnmsagar@users.noreply.github.com>
2023-03-03 10:01:45 +0530

    Update NFT_Marketplace.sol

--- NFT_Marketplace.sol
+++ NFT_Marketplace.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
